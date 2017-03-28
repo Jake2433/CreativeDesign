3e5be060c4a9349db3a783c0d8895c1ba4e06d4a
김용범 <kyb2433@gmail.com>
2017-03-28 12:29:35 +0900

    SmartFarm-android

---
+++ SmartFarm/app/src/main/java/com/example/kyb24/smartfarm/MenuPage.java
@@ -0,0 +1,16 @@
+package com.example.kyb24.smartfarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kyb24 on 2017-03-27.
+ */
+
+public class MenuPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
